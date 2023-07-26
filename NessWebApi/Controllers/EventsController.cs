e10ca85c087575592b6f715e66d40ef2eb36975f
Magdalena-Paicu <magdalena.paicu@ness.com>
2023-07-26 12:29:49 +0300

    first commit

---
+++ NessWebApi/Controllers/EventsController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NessWebApi.Data;
+using NessWebApi.Models;
+
+namespace NessWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly DbContextNessApp _dbContextNessApp;
+
+        public EventsController(IWebHostEnvironment webHostEnvironment, DbContextNessApp dbContextNessApp)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _dbContextNessApp = dbContextNessApp;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEvents()
+        {
+            var allEvents = await _dbContextNessApp.Events.ToListAsync();
+            return Ok(allEvents);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetEventById(int id)
+        {
+            var ev = await _dbContextNessApp.Events.FindAsync(id);
+            if (ev == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ev);
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> CreateNewEvent([FromForm] Event newEvent, IFormFile file)
+        //{
+        //    if (file != null && file.Length > 0)
+        //    {
+        //        string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+        //        if (!Directory.Exists(uploadsFolder))
+        //        {
+        //            Directory.CreateDirectory(uploadsFolder);
+        //        }
+
+        //        string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+        //        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+        //        using (var stream = new FileStream(filePath, FileMode.Create))
+        //        {
+        //            await file.CopyToAsync(stream);
+        //        }
+
+           
+        //        if (!_dbContextNessApp.Events.Any())
+        //        {
+        //            newEvent.Id = 0;
+        //        }
+        //        else
+        //        {
+        //            int maxExistingId = _dbContextNessApp.Events.Max(e => e.Id);
+        //            newEvent.Id = maxExistingId + 1;
+        //        }
+
+        //        newEvent.ImageUrl = "/uploads/" + uniqueFileName;
+
+        //        _dbContextNessApp.Events.Add(newEvent);
+        //        await _dbContextNessApp.SaveChangesAsync();
+
+        //        return Ok(newEvent);
+        //    }
+
+        //    return BadRequest("No file or file is empty.");
+        //}
+
+
+
+
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateEvent(int id, [FromForm] Event updateEvent, IFormFile file)
+        {
+            var ev = await _dbContextNessApp.Events.FindAsync(id);
+            if (ev == null)
+            {
+                return NotFound();
+            }
+
+            ev.Title = updateEvent.Title;
+            // Actualizează restul proprietăților evenimentului
+
+            if (file != null && file.Length > 0)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                ev.ImageUrl = "/uploads/" + uniqueFileName;
+            }
+
+            await _dbContextNessApp.SaveChangesAsync();
+
+            return Ok(ev);
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public async Task<IActionResult> DeleteEvent(int id) {
+            var ev = await _dbContextNessApp.Events.FindAsync(id);
+            if (ev != null)
+            {
+                _dbContextNessApp.Events.Remove(ev);
+                await _dbContextNessApp.SaveChangesAsync();
+                return Ok(ev);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ NessWebApi/Controllers/FileUploadsController.cs
@@ -0,0 +1,136 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NessWebApi.Data;
+using NessWebApi.Models;
+
+namespace NessWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadsController : Controller
+    {
+        public static IWebHostEnvironment _webHostEnvironment;
+        private readonly DbContextNessApp _dbContextNessApp;
+
+
+        public FileUploadsController(IWebHostEnvironment webHostEnvironment, [FromServices] DbContextNessApp dbContextNessApp)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _dbContextNessApp = dbContextNessApp;
+        }
+
+        [HttpPost]
+        public async Task<string> Post([FromForm] FileUpload fileUpload)
+        {
+            try
+            {
+                if (fileUpload.files != null && fileUpload.files.Length > 0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\uploads\\";
+
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(path + fileUpload.files.FileName))
+                    {
+                        fileUpload.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                    }
+
+                    var uploadedFile = new UploadedFile
+                    {
+                        FileName = fileUpload.files.FileName,
+                        UploadDateTime = DateTime.Now,
+                        FileSize = fileUpload.files.Length,
+                        ImageUrl = "/uploads/" + fileUpload.files.FileName
+                    };
+
+                    _dbContextNessApp.UploadedFiles.Add(uploadedFile);
+                    await _dbContextNessApp.SaveChangesAsync();
+
+
+                    return "Upload Done.";
+                }
+                else
+                {
+                    return "Failed!";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        [HttpGet("{fileName}")]
+        public async Task<IActionResult> Get([FromRoute] string fileName)
+        {
+            string path = _webHostEnvironment.WebRootPath + "\\uploads\\";
+            var filePath = path + fileName + ".png";
+            if (System.IO.File.Exists(filePath))
+            {
+                byte[] b = System.IO.File.ReadAllBytes(filePath);
+                return File(b, "image/png");
+            }
+            return null;
+        }
+
+        [HttpDelete("delete/{fileName}")]
+        public async Task<IActionResult> DeleteFile(string fileName)
+        {
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", fileName);
+
+            try
+            {
+                // Căutați înregistrarea în baza de date bazată pe numele fișierului
+                var uploadedFile = await _dbContextNessApp.UploadedFiles.FirstOrDefaultAsync(file => file.FileName == fileName);
+
+                if (uploadedFile != null)
+                {
+                    // Eliminați înregistrarea din baza de date
+                    _dbContextNessApp.UploadedFiles.Remove(uploadedFile);
+                    await _dbContextNessApp.SaveChangesAsync();
+
+                    if (System.IO.File.Exists(path))
+                    {
+                        // Ștergeți fișierul fizic din folderul uploads
+                        System.IO.File.Delete(path);
+                        return Ok("Fișierul a fost șters cu succes.");
+                    }
+                }
+                else
+                {
+                    return NotFound("Fișierul nu a fost găsit în baza de date.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return NotFound("Fișierul nu a fost găsit pe disc.");
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllImages()
+        {
+            try
+            {
+                var uploadedFiles = await _dbContextNessApp.UploadedFiles.ToListAsync();
+
+                return Ok(uploadedFiles);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+
+
+}
